ef70a851dffff9ecf35b3a0e84beb65c2661b74a
jiroquijano <jiroquijano@gmail.com>
2020-10-26 13:56:04 +0800

    fixed reducer result when json or emv was updated

--- src/reducers/inputReducer.js
+++ src/reducers/inputReducer.js
@@ -1,10 +1,9 @@
 const inputReducer = (state,action)=>{
     switch(action.type){
         case 'UPDATE_JSON_INPUT':
-            return action.json ? {...state, json: action.json} : state;
+            return action.json ? {json: action.json} : state;
         case 'UPDATE_EMV_INPUT':
-            console.log(action.emv)
-            return action.emv ? {...state, emv: action.emv} : state;
+            return action.emv ? {emv: action.emv} : state;
         default:
             return state;
     }
