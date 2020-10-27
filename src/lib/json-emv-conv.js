bade1bc0b301f0f2c29dd8abb029e63bd98b4b8f
jiroquijano <jiroquijano@gmail.com>
2020-10-27 10:59:08 +0800

    converted input object to array to guarantee order of input fields

--- src/lib/json-emv-conv.js
+++ src/lib/json-emv-conv.js
@@ -7,29 +7,62 @@ const padPayloadLength = (payload) =>{
     return payload.length < 10 ? `0${payload.length}` : payload.length;
 }
 
-//EMV format: `[KEY][PAYLOAD LENGTH][PAYLOAD]`
-const transformToEMVFormat = (key, payload) =>{
-    if(typeof(payload) === 'string'){ //for simple string payload
-        return `${keyToIDMap[key]}${padPayloadLength(payload)}${payload}`;
-    }else if(typeof(payload) ==='object'){ //for nested object payload
-        const rootkey = keyToIDMap[key];
-        const mappedChildObject = _.mapKeys(payload,(_val,_key)=>transformToEMVFormat(_key,_val));
-        const aggregatedChildObjectKeys = Object.keys(mappedChildObject).join('');
-        return `${rootkey}${padPayloadLength(aggregatedChildObjectKeys)}${aggregatedChildObjectKeys}`;
-    }
-};
-
 const calculateAndFormatCRC = (input) =>{
     const crc = crc16ccitt(`${input}6304`).toString(16).toUpperCase(); //[KEY][LENGTH] of CRC part of CRC payload
     const paddedCrc = _.padStart(crc,4,'0');
     return transformToEMVFormat('crc',paddedCrc);
 }
 
+//EMV format: `[KEY][PAYLOAD LENGTH][PAYLOAD]`
+const transformToEMVFormat = (key, payload) =>{
+    if(typeof(payload) === 'string'){ //for simple string payload
+        return `${keyToIDMap[key]}${padPayloadLength(payload)}${payload}`;
+    }else if(typeof(payload) ==='object'){ //for nested array payload
+        const rootkey = keyToIDMap[key];
+        const aggregatedChildArray = payload.reduce((acc,curr)=>{
+            return `${acc}${transformToEMVFormat(curr[0],curr[1])}`;
+        },'');
+        return `${rootkey}${padPayloadLength(aggregatedChildArray)}${aggregatedChildArray}`;
+    }
+}
+
+const convertObjectToArray = (input)=>{ //transform object to array to guarantee order of fields
+    const resultingArray = [];
+    ['pfi','pim','mait','mcc','txCurrency','txAmt','cc','merName','merCity','additional'].forEach((key)=>{
+        if(input[key]){
+            if(key === 'mait'){
+                const innerArray = [];
+                ['guid','acqid','merid','pnflags'].forEach((innerKey)=>{
+                    if(input[key][innerKey]){
+                        innerArray.push([innerKey, input[key][innerKey]]);
+                    }
+                });
+                resultingArray.push([key, innerArray])
+            }else if(key === 'additional'){
+                const innerArray = [];
+                ['refLabel','termLabel'].forEach((innerKey)=>{
+                    if(input[key][innerKey]){
+                        innerArray.push([innerKey, input[key][innerKey]]);
+                    }
+                });
+                resultingArray.push([key, innerArray]);
+            }else if(keyToIDMap[key] === undefined){
+                console.log('invalid key');
+            }else{
+                resultingArray.push([key,input[key]]);
+            }
+        }
+    });
+    return resultingArray;
+}
+
 const convertObjectToEMVCode = (input) =>{
     if(_.isEmpty(input)) return '';
-    const mappedObject = _.mapKeys(input,(val,key)=>transformToEMVFormat(key,val));
-    const emvString = Object.keys(mappedObject).join('');
-    const crcString= calculateAndFormatCRC(emvString);
+    const inputArray = convertObjectToArray(input); //to guarantee input field order (this is crucial for crc), transform object to array
+    const emvString = inputArray.reduce((acc, curr)=>{
+        return `${acc}${transformToEMVFormat(curr[0],curr[1])}`
+    },'');
+    const crcString = calculateAndFormatCRC(emvString);
     return emvString+crcString;
 }
 
